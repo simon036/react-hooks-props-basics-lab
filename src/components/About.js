ed816220717eae18e66e8a480b0dad5c591f7852
Simon <ndungusimon564@gmail.com>
2025-01-16 09:25:21 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ function About({bio, github ,linkedin}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {/* <p>{bio}</p> */}
       {bio && bio.trim() !== "" && <p>{bio}</p>} 
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
